ae289409240544d2a8dab9516aef0b906032ef3f
Jason Constam <callmebob@gmail.com>
2019-05-26 22:54:35 +0200

    Started work on adding doxygen comments in data.hpp

--- include/data.hpp
+++ include/data.hpp
@@ -20,28 +20,41 @@
 
 #define ZMQ_DATA_MSG_DATA_PTR( msg )    ( &( ( ( uint8_t* ) msg )[ DATA_TYPE_RAW_SIZE ] ) )
 
+/// @brief Internal data type
 typedef enum
 {
+    /// @brief uint16_t
     DATA_TYPE_UINT16,
+    /// @brief uint16_t
     DATA_TYPE_UINT32,
+    /// @brief float
     DATA_TYPE_FLOAT
 } DATA_TYPE;
 
+/// @brief Raw ZeroMQ data for a uint16_t data point
 typedef struct
 {
+    /// @brief Data type value
     DATA_TYPE type;
+    /// @brief Raw uint16_t value
     uint16_t value;
 } RAWDATA_UINT16;
 
+/// @brief Raw ZeroMQ data for a uint32_t data point
 typedef struct
 {
+    /// @brief Data type value
     DATA_TYPE type;
+    /// @brief Raw uint32_t value
     uint32_t value;
 } RAWDATA_UINT32;
 
+/// @brief Raw ZeroMQ data for a float data point
 typedef struct
 {
+    /// @brief Data type value
     DATA_TYPE type;
+    /// @brief Raw float value
     float value;
 } RAWDATA_FLOAT;
 
